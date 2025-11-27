fea734551a38933b485858b374485636b8a2fa8d
sajid sabila <sajid.sabila@gmail.com>
2025-11-27 12:05:53 +0700

    fix error route

--- src/pages/Student/studentReport.jsx
+++ src/pages/Student/studentReport.jsx
@@ -21,19 +21,19 @@ const StudentReport = () => {
 
             <div className="flex">
               <img src="/assets/smile_image/icon-1.png" className="w-15 h-15 relative z-1 top-2 left-3"/>
-              <Link to="/students-report/history-absensi" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
+              <Link to="/student/students-report/history-absensi" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
                   <span className="ms-13">Student Attendance</span>
               </Link>
           </div>
           <div className="flex">
                <img src="/assets/smile_image/icon-6.png" className="w-15 h-15 relative z-1 top-2 left-3"/>
-            <Link to="/students-report/fees" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
+            <Link to="/student/students-report/fees" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
              <span className="ms-13">Fees</span>
             </Link>
           </div>
           <div className="flex">
               <img src="/assets/smile_image/icon-5.png" className="w-15 h-15 relative z-1 top-2 left-3"/>
-            <Link to="/students-report/evaluasi-semester" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
+            <Link to="/student/students-report/evaluasi-semester" className="relative right-15 z-0 w-full md:w-72 bg-black text-white py-3 px-6 font-bold mt-4 rounded-lg shadow-xl hover:scale-105 transition">
             <span className="ms-13">Evaluation Semester</span>      
              </Link>
              </div>
